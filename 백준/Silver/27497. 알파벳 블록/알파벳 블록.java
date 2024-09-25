9071e6f5db453535c1dfea3b2eaff01465434b91
garamflow <125892312+garamflow@users.noreply.github.com>
2024-09-25 11:36:48 +0900

    [Silver II] Title: 알파벳 블록, Time: 644 ms, Memory: 256100 KB -BaekjoonHub

---
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 버튼 누른 횟수와 그 횟수에 맞게 Deque 구현
+        // linkedList는 최근에 앞에 저장했는지 뒤에 저장했는지를 기록하는 역할
+        int N = Integer.parseInt(st.nextToken());
+        Deque<String> deque = new LinkedList<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            if (x == 1) {
+                deque.addLast(st.nextToken());
+                linkedList.addLast("last");
+            } else if (x == 2) {
+                deque.addFirst(st.nextToken());
+                linkedList.addLast("first");
+            } else if (x == 3) {
+                if (!linkedList.isEmpty()) {
+                    if (linkedList.removeLast().equals("last")) {
+                        deque.removeLast();
+                    } else {
+                        deque.removeFirst();
+                    }
+                }
+            }
+        }
+
+        if(deque.isEmpty()){
+            System.out.println(0);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (String s : deque) {
+                sb.append(s);
+            }
+            System.out.println(sb);
+        }
+    }
+}
