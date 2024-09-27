ca77e59f9030d10cca6f06c3f2dd6a65a9585be7
garamflow <125892312+garamflow@users.noreply.github.com>
2024-09-27 16:22:12 +0900

    [Silver V] Title: 국회의원 선거, Time: 96 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,29 +1,29 @@
+// BufferedReader 부분만 리팩토링
+import java.util.*;
 import java.io.*;
-import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int N = Integer.parseInt(br.readLine());
         int dasom = Integer.parseInt(br.readLine());
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 1; i < N; i++) {
-            pq.offer(-Integer.parseInt(br.readLine()));
+            pq.offer(Integer.parseInt(br.readLine()));
         }
-        
+
         int count = 0;
-        
-        while (!pq.isEmpty() && dasom <= -pq.peek()) {
-            int vote = -pq.poll();
+
+        while (!pq.isEmpty() && dasom <= pq.peek()) {
+            int vote = pq.poll();
             vote--;
-            pq.offer(-vote);
+            pq.offer(vote);
             dasom++;
             count++;
         }
-        
+
         System.out.println(count);
         br.close();
     }
