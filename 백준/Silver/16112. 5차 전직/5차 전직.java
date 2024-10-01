8d05d4f5938a56420329f5d27725f739a27fc7ca
garamflow <125892312+garamflow@users.noreply.github.com>
2024-10-01 12:21:40 +0900

    [Silver II] Title: 5차 전직, Time: 548 ms, Memory: 62832 KB -BaekjoonHub

---
+++ 백준/Silver/16112. 5차 전직/5차 전직.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.offer(Long.parseLong(st.nextToken()));
+        }
+
+        Long acane = 0L;
+
+        for (int i = 0; i < n; i++) {
+            int active = Math.min(i, k);
+            acane += pq.poll() * active;
+        }
+
+        System.out.println(acane);
+    }
+}
